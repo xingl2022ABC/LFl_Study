c6ce7a622a0e5a39e244127df92835bccdafe78b
xingl2022ABC <2277198055@qq.com>
2022-08-04 11:51:46 +0800

    null
    Null

---
+++ Test.java
@@ -0,0 +1,12 @@
+package New_Test;
+
+import java.util.Random;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Student s = new Student("Éç»á", 25, 'ÄÐ');
+        System.out.println(s);
+    }
+    }
+
